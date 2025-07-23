3b591f32d076dd8561c51336bdc883a6ad6c14e7
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-07-23 07:18:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-    },
+  reactStrictMode: true,
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
 };
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer(nextConfig);
