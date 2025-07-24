187e3d1848cd2f40615cc477bfeda7ec5aed290d
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-07-24 15:14:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["raw.githubusercontent.com"],
+    domains: 
+      ["raw.githubusercontent.com"],
+       ["i.ibb.co"],
   },
 };
 
