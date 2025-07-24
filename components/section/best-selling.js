7b70e85f8553746e769ac72203b32f0cbbafb760
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-07-24 13:45:42 +0700

    Update best-selling.js

--- components/section/best-selling.js
+++ components/section/best-selling.js
@@ -2,24 +2,22 @@ const BestSelling = ({ products }) => {
     return (
         <div className="flex flex-col overflow-x-auto">
             <ul>
-                <li className="flex flex-row items-center justify-between py-3 px-3 bg-gray-200">
-                    <div className="flex flex-row gap-x-4 items-center">
-                        <div className="rounded-full bg-red-300 w-7 h-7 flex items-center justify-center">
-                            1
+                {products?.map((product, index) => (
+                    <li
+                        key={index}
+                        className={`flex flex-row items-center justify-between py-3 px-3 ${
+                            index % 2 === 0 ? "bg-gray-200" : "bg-gray-100"
+                        }`}
+                    >
+                        <div className="flex flex-row gap-x-4 items-center">
+                            <div className="rounded-full bg-red-300 w-7 h-7 flex items-center justify-center font-bold">
+                                {index + 1}
+                            </div>
+                            <h1>{product.name}</h1>
                         </div>
-                        <h1>Discord</h1>
-                    </div>
-                    <p>23 ชิ้น</p>
-                </li>
-                <li className="flex flex-row items-center justify-between py-3 px-3 bg-gray-100">
-                    <div className="flex flex-row gap-x-4 items-center">
-                        <div className="rounded-full bg-red-300 w-7 h-7 flex items-center justify-center">
-                            2
-                        </div>
-                        <h1>Youtube</h1>
-                    </div>
-                    <p>19 ชิ้น</p>
-                </li>
+                        <p>{product.sold} ชิ้น</p>
+                    </li>
+                ))}
             </ul>
         </div>
     );
