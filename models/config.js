6e4839fc3a67675a0f44b79e13246b3047962f0b
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-07-24 19:00:35 +0700

    Update config.js

--- models/config.js
+++ models/config.js
@@ -1,98 +1,129 @@
 import mongoose from "mongoose";
 
 const ConfigSchema = mongoose.Schema({
-    website_title: {
-        type: String,
-        default: "Skitzer",
+  website_title: {
+    type: String,
+    default: "Skitzer",
+  },
+  website_name: {
+    type: String,
+    default: "Sktz",
+  },
+  website_desc: {
+    type: String,
+    default: "Skitzer Solution. E-Commerce Website Provider.",
+  },
+  website_icon: {
+    type: String,
+    default: "https://media.discordapp.net/attachments/717327142978977834/1060896307235004467/favicon.png",
+  },
+  website_logo: {
+    type: String,
+    default: "",
+  },
+  website_banner: {
+    type: String,
+    default: "https://dummyimage.com/1100x240",
+  },
+  website_image: {
+    type: String,
+    default: "",
+  },
+  announcement: {
+    type: String,
+    default: "",
+  },
+  payment: {
+    truemoney_gift: {
+      type: Boolean,
+      default: true,
     },
-    website_name: {
-        type: String,
-        default: "Sktz",
+    truemoney_gift_fee_type: {
+      type: String,
+      enum: ["fixed", "percent"],
+      default: "fixed",
     },
-    website_desc: {
-        type: String,
-        default: "Skitzer Solution. E-Commerce Website Provider.",
+    truemoney_gift_fee_value: {
+      type: Number,
+      default: 0,
     },
-    website_icon: {
-        type: String,
-        default: "https://media.discordapp.net/attachments/717327142978977834/1060896307235004467/favicon.png",
+
+    truemoney_qr: {
+      type: Boolean,
+      default: false,
     },
-    website_logo: {
-        type: String,
-        default: "",
+    truemoney_qr_fee_type: {
+      type: String,
+      enum: ["fixed", "percent"],
+      default: "fixed",
     },
-    website_banner: {
-        type: String,
-        default: "https://dummyimage.com/1100x240",
+    truemoney_qr_fee_value: {
+      type: Number,
+      default: 0,
     },
-    website_image: {
-        type: String,
-        default: "",
+
+    truemoney: {
+      type: Boolean,
+      default: false,
     },
-    announcement: {
-        type: String,
-        default: "",
+
+    promptpay_qr: {
+      type: Boolean,
+      default: false,
     },
-    payment: {
-        truemoney_gift: {
-            type: Boolean,
-            default: true,
-        },
-        truemoney_qr: {
-            type: Boolean,
-            default: false,
-        },
-        truemoney: {
-            type: Boolean,
-            default: false,
-        },
-        promptpay_qr: {
-            type: Boolean,
-            default: false,
-        },
-        promptpay_id: {
-            type: String,
-            default: "",
-        },
-        bank_account_name_th: {
-            type: String,
-            default: "",
-        },
-        bank_account_name_en: {
-            type: String,
-            default: ""
-        },
-        truemoney_phone: {
-            type: String,
-            maxLength: 10,
-            default: "",
-        },
+    promptpay_qr_fee_type: {
+      type: String,
+      enum: ["fixed", "percent"],
+      default: "fixed",
     },
-    style: {
-        primary_color: {
-            type: String,
-            default: "#5c6ac4",
-        },
-        background_image: {
-            type: String,
-            default:
-                "https://media.discordapp.net/attachments/829046235527905334/1073568814475776140/roroto-sic-panda-chapeaute-miror.png",
-        },
-        background_color: {
-            type: String,
-            default: "#f0f0f0e6",
-        },
+    promptpay_qr_fee_value: {
+      type: Number,
+      default: 0,
     },
-    social: {
-        discord_url: {
-            type: String,
-            default: "",
-        },
-        facebook_url: {
-            type: String,
-            default: "https://www.facebook.com/skitzer.xyz",
-        },
+
+    promptpay_id: {
+      type: String,
+      default: "",
     },
+    bank_account_name_th: {
+      type: String,
+      default: "",
+    },
+    bank_account_name_en: {
+      type: String,
+      default: "",
+    },
+    truemoney_phone: {
+      type: String,
+      maxLength: 10,
+      default: "",
+    },
+  },
+  style: {
+    primary_color: {
+      type: String,
+      default: "#5c6ac4",
+    },
+    background_image: {
+      type: String,
+      default:
+        "https://media.discordapp.net/attachments/829046235527905334/1073568814475776140/roroto-sic-panda-chapeaute-miror.png",
+    },
+    background_color: {
+      type: String,
+      default: "#f0f0f0e6",
+    },
+  },
+  social: {
+    discord_url: {
+      type: String,
+      default: "",
+    },
+    facebook_url: {
+      type: String,
+      default: "https://www.facebook.com/skitzer.xyz",
+    },
+  },
 });
 
 export default mongoose.models.Config || mongoose.model("Config", ConfigSchema);
