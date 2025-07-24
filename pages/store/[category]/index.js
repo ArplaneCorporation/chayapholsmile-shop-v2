3ca465be9eb95bdd8fc83bd4e534d5dffc372613
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-07-24 15:51:12 +0700

    Update index.js

--- pages/store/[category]/index.js
+++ pages/store/[category]/index.js
@@ -8,100 +8,105 @@ import LoadingSpiner from "../../../components/ui/loader/spiner";
 import Image from "next/image";
 
 const DynamicCategory = (props) => {
-    const router = useRouter();
-    const cid = router.query.category;
+  const router = useRouter();
+  const cid = router.query.category;
 
-    const [products, setProducts] = useState(props.products);
-    const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState(props.products);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const getAllProducts = async () => {
-            const { data } = await axios.get(`/api/products?cid=${cid}`);
-            setProducts(data.products);
-            setLoading(false);
-        };
-        getAllProducts().catch(() => {
-            console.error;
-            setLoading(false);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const { data } = await axios.get(`/api/products?cid=${cid}`);
+        setProducts(data.products);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return (
-        <Layout>
-            <main className="max-w-[1150px] sm:px-[17px] pb-4 sm:pb-[25px] pt-24 md:pt-28 mx-auto items-center">
-                <h1 className="text-center text-2xl md:text-4xl font-bold mb-4 md:mb-8">
-                    หมวดหมู่ {props.category?.name}
-                </h1>
-                <section
-                    id="banner"
-                    className="flex justify-center items-center aspect-[16/3.5] relative overflow-hidden md:rounded-lg md:mx-2 mb-4 md:mb-6 lg:mb-8"
-                >
-                    <Image
-                        alt="homepage_banner"
-                        src={"https://dummyimage.com/1100x240"}
-                        draggable="false"
-                        fill
-                        className="select-none object-cover"
-                    />
-                </section>
-                {loading ? (
-                    <LoadingSpiner />
-                ) : (
-                    <div className="px-4 md:px-2 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-start gap-2 md:gap-4 lg:gap-6">
-                        {products?.map((product, i) => (
-                            <ProductCard key={i} product={product} />
-                        ))}
-                    </div>
-                )}
-            </main>
-        </Layout>
-    );
+    if (cid) {
+      getAllProducts();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cid]);
+
+  return (
+    <Layout>
+      <main className="max-w-[1150px] sm:px-[17px] pb-4 sm:pb-[25px] pt-24 md:pt-28 mx-auto items-center">
+        <h1 className="text-center text-2xl md:text-4xl font-bold mb-4 md:mb-8">
+          หมวดหมู่ {props.category?.name}
+        </h1>
+
+        {props.configs?.website_banner && (
+          <section
+            id="banner"
+            className="flex justify-center items-center aspect-[16/3.5] relative overflow-hidden md:rounded-lg md:mx-2 mb-4 md:mb-6 lg:mb-8"
+          >
+            <Image
+              alt="homepage_banner"
+              src={props.configs.website_banner}
+              draggable="false"
+              fill
+              className="select-none object-cover"
+              priority
+            />
+          </section>
+        )}
+
+        {loading ? (
+          <LoadingSpiner />
+        ) : (
+          <div className="px-4 md:px-2 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-start gap-2 md:gap-4 lg:gap-6">
+            {products?.map((product, i) => (
+              <ProductCard key={i} product={product} />
+            ))}
+          </div>
+        )}
+      </main>
+    </Layout>
+  );
 };
 
 export default DynamicCategory;
 
-// export { getServerSideProps } from "../../../utils/get-init-data";
-
 export const getServerSideProps = withInitProps(async (ctx) => {
-    try {
-        const cid = ctx.params.category;
+  try {
+    const cid = ctx.params.category;
 
-        const nextRequestMeta =
-            ctx.req[
-                Reflect.ownKeys(ctx.req).find(
-                    (s) => String(s) === "Symbol(NextRequestMeta)"
-                )
-            ];
-        const protocal = nextRequestMeta._protocol;
+    const nextRequestMeta =
+      ctx.req[
+        Reflect.ownKeys(ctx.req).find((s) => String(s) === "Symbol(NextRequestMeta)")
+      ];
+    const protocol = nextRequestMeta._protocol;
+    const host = ctx.req.headers.host;
 
-        const res = await axios.get(
-            `${protocal}://${ctx.req.headers.host}/api/categories/${cid}`
-        );
+    const resCategory = await axios.get(`${protocol}://${host}/api/categories/${cid}`);
 
-        if (res.data.category?.type === "ID_PASS") {
-            return {
-                redirect: {
-                    destination: `/store/idpass/${cid}`,
-                    permanent: true,
-                },
-            };
-        }
-
-        const { data } = await axios.get(
-            `${protocal}://${ctx.req.headers.host}/api/products?cid=${cid}`
-        );
-
-        return {
-            props: {
-                products: data.products,
-                category: res.data.category,
-            },
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            notFound: true,
-        };
+    if (resCategory.data.category?.type === "ID_PASS") {
+      return {
+        redirect: {
+          destination: `/store/idpass/${cid}`,
+          permanent: true,
+        },
+      };
     }
+
+    const resProducts = await axios.get(`${protocol}://${host}/api/products?cid=${cid}`);
+    const resConfigs = await axios.get(`${protocol}://${host}/api/configs`);
+
+    return {
+      props: {
+        products: resProducts.data.products,
+        category: resCategory.data.category,
+        configs: resConfigs.data,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    };
+  }
 });
